55857a76ce23506e5b3e5bbf8798c031a154e18b
Nikith_Deepthimahanthi <nikithd235@gmail.com>
2020-08-18 16:15:07 +0530

    nwonesssss

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var css= document.querySelector("h3");
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var body=document.getElementById('grad')
+function setgradient(){
+ body.style.background= 
+ "linear-gradient(to right,"
+ +color1.value
+ + "," +
+ color2.value+")";
 
-function inputLength() {
-	return input.value.length;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-}
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
+color1.addEventListener("input", setgradient);
+
+color2.addEventListener("input", setgradient);
+
+
+//click button 
+function clickMe(){
+	var num1=Math.random()*1000;
+	var num2=Math.random()*1000;
+	var num3=Math.random()*1000;
+	var num4=Math.random()*1000;
+	var num5=Math.random()*1000;
+	var num6=Math.random()*1000;
+	num6=Math.floor(num6);
+	num5=Math.floor(num5);
+	num4=Math.floor(num4);
+	num3=Math.floor(num3);
+	num2=Math.floor(num2);
+	num1=Math.floor(num1);
+	if (num1<=255 && num2<=255 && num3<=255) {
+		body.style.background="linear-gradient(to right,"
+        + "rgb("+num1+","+num2+","+num3+")"+","
+        +  "rgb("+num4+","+num5+","+num6+")"
 	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
+	else{
+		clickMe()
 	}
+
+
 }
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress );
+var button= document.querySelector(".butt1");
+button.addEventListener("click", clickMe);
